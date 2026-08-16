c0c1dc9387b7be381de8c58817c66a9547851efd
jkotieno <42593770+jkotieno@users.noreply.github.com>
2026-08-16 17:25:08 -0400

    feat: register no-save image output sink

--- media_output/__init__.py
+++ media_output/__init__.py
@@ -1,13 +1,15 @@
 """Temporary media output nodes for Sweet Tea Studio transports."""
 
-from .nodes_media_output import SweetTeaExecutionReceipt, SweetTeaPreviewVideo
+from .nodes_media_output import SweetTeaExecutionReceipt, SweetTeaPreviewImage, SweetTeaPreviewVideo
 
 NODE_CLASS_MAPPINGS = {
+    "SweetTeaPreviewImage": SweetTeaPreviewImage,
     "SweetTeaPreviewVideo": SweetTeaPreviewVideo,
     "SweetTeaExecutionReceipt": SweetTeaExecutionReceipt,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "SweetTeaPreviewImage": "Sweet Tea Preview Image",
     "SweetTeaPreviewVideo": "Sweet Tea Preview Video",
     "SweetTeaExecutionReceipt": "Sweet Tea Execution Receipt",
 }
@@ -15,6 +17,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
+    "SweetTeaPreviewImage",
     "SweetTeaPreviewVideo",
     "SweetTeaExecutionReceipt",
 ]
